caa73236368bad106c3f4ebb1cecbb82ad8978e6
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-05-20 13:03:52 +0530

    UI intro

--- 1-web-UI/index.js
+++ 1-web-UI/index.js
@@ -13,7 +13,7 @@ let infoBtn = document.querySelector('.btn-info')
 
 
 infoBtn.addEventListener('click', e => {
-    alertBox.innerHTML="start training"
+    alertBox.innerHTML = "start training"
 })
 showBtn.addEventListener('click', e => {
     alertBox.style.display = ''
@@ -21,3 +21,36 @@ showBtn.addEventListener('click', e => {
 hideBtn.addEventListener('click', e => {
     alertBox.style.display = 'none'
 })
+
+
+
+//----------------------------------------------
+// using DOM API  + XHR/Fetch API
+//----------------------------------------------
+
+let todosBtn = document.querySelector('.btn-dark');
+todosBtn.addEventListener('click', e => {
+    let apiUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=5';
+    let promise = fetch(apiUrl)
+    promise
+        .then(response => response.json())
+        .then(todos => {
+            let liEles = todos.map(todo => {
+                return `<li class="list-group-item ${todo.completed ? 'bg-success' : ''}">${todo.id} - ${todo.title} - ${todo.completed}</li>`
+            });
+            document.getElementById('todo-list').innerHTML = liEles.join(" ")
+        })
+})
+
+
+//----------------------------------------------
+// using DOM API  + Timer API
+//----------------------------------------------
+
+setInterval(() => {
+    let timeEle = document.getElementById('time');
+    timeEle.innerHTML = new Date().toLocaleTimeString()
+}, 1000);
+
+
+
